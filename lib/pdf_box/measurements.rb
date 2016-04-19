bd443babcd732cf1952955fa6b8720ca756f864a
Roman Simecek <roman@good2go.ch>
2016-04-19 09:34:34 +0200

    Initial commit.

---
+++ lib/pdf_box/measurements.rb
@@ -0,0 +1,7 @@
+require "pdf_box/measurements/version"
+
+module PdfBox
+  module Measurements
+    # Your code goes here...
+  end
+end
---
+++ lib/pdf_box/measurements/version.rb
@@ -0,0 +1,5 @@
+module PdfBox
+  module Measurements
+    VERSION = "0.1.0"
+  end
+end
